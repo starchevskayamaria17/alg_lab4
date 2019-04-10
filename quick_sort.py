b54c55e09e74b1437506464bd69f2d74540b0b79
starchevskayamaria17 <m.starchevskaia@g.nsu.ru>
2019-04-10 10:05:52 +0700

    My quick sorted

--- quick_sort.py
+++ quick_sort.py
@@ -1,8 +1,8 @@
-def qsort(a):
+def quick_sort(a):
     if len(a) <= 1:
         return a
     else:
         pivot = a[0]
         lesser = [i for i in a[1:] if i < pivot]
         greater = [i for i in a[1:] if i >= pivot]
-    return qsort(lesser) + [pivot] + qsort(greater)
+    return quick_sort(lesser) + [pivot] + quick_sort(greater)

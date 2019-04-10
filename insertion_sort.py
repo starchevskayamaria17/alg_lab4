8fb083138ac3790527207c89c4d8651cc0c05551
starchevskayamaria17 <m.starchevskaia@g.nsu.ru>
2019-04-10 09:38:48 +0700

    Save my sorting

--- insertion_sort.py
+++ insertion_sort.py
@@ -2,4 +2,10 @@
 # -*- coding: utf-8 -*-
 
 def insertion_sort(a):
-    pass
+    for i in range(len(a)):
+        tmp = a[i]
+        x = i - 1
+        while x >= 0 and tmp < a[x]:
+            a[x+1], a[x] = a[x], a[x+1]
+            x -= 1
+    return(a)
--- selection_sort.py
+++ selection_sort.py
@@ -2,4 +2,12 @@
 # -*- coding: utf-8 -*-
 
 def selection_sort(a):
-    pass
+    for i in range(len(a) - 1):
+        tmp = i
+        x = i + 1
+        while x < len(a):
+            if a[x] < a[tmp]:
+                tmp = x
+            x += 1
+        a[i], a[tmp] = a[tmp], a[i]
+    return a
